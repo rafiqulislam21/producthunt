c37fd3a41e0589abce509f4be0469a825c916371
Md. Rafiqul Islam <ribijoy19@gmail.com>
2020-11-04 10:09:09 +0600

    web part complete

--- producthunt/urls.py
+++ producthunt/urls.py
@@ -13,12 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
+from producthunt import settings
 from products import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('accounts/', include('accounts.urls')),
-]
+    path('products/', include('products.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
